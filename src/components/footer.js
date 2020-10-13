61936174f920f2750f4b58c52ad62511014f7f1a
g00376679@gmit.ie <g00376679@gmit.ie>
2020-10-13 03:58:29 +0100

    My Lab upload

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export class Footer extends React.Component{
+
+render(){
+    return(
+        <div>
+            <hi>This is my Footer Component.</hi>
+        </div>
+    );
+}
+
+}
---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export class Header extends React.Component{
+
+render(){
+    return(
+        <div>
+            <hi>This is my header Component.</hi>
+        </div>
+    );
+}
+
+}
